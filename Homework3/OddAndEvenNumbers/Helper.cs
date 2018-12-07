184e711114ca4d7ae21cd81684bb206969f1a6e4
Monica Danca <mdanca@endava.com>
2018-12-07 10:23:15 +0200

    add homework 3

---
+++ Homework3/OddAndEvenNumbers/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddAndEvenNumbers
+{
+    public class Helper
+    {
+        public static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ Homework3/OddAndEvenNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddAndEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 0, 10, 23, 14, 9043, 12, 3, 5, -10 };
+            int oddNumbers = 0, evenNumbers = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0) evenNumbers++;
+                else oddNumbers++;
+            }
+
+            int[] oddNumbersArray = new int[oddNumbers];
+            int[] evenNumbersArray = new int[evenNumbers];
+
+            oddNumbers = 0;
+            evenNumbers = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0) evenNumbersArray[evenNumbers++] = numbers[i];
+                else oddNumbersArray[oddNumbers++] = numbers[i];
+            }
+
+            Console.WriteLine("Odd numbers are: ");
+            Helper.PrintArray(oddNumbersArray);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Even numbers are: ");
+            Helper.PrintArray(evenNumbersArray);
+
+            Console.ReadKey();
+        }
+
+    }
+}
