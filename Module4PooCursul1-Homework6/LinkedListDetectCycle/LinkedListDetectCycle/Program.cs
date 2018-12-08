6853c489de3f905e00ad27843e3eac5a27b95c64
Monica0828 <danca.monica95@gmail.com>
2018-12-08 10:28:54 +0200

    Added liked list problem

---
+++ Module4PooCursul1-Homework6/LinkedListDetectCycle/LinkedListDetectCycle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDetectCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList list = new SingleLinkedList();
+
+            Node node1 = list.AddFirst(1);
+            Node node2 = list.AddAfter(node1, 2);
+            Node node3 = list.AddAfter(node2, 3);
+            Node node4 = list.AddAfter(node3, 4);
+            Node node5 = list.AddAfter(node4, 5);
+
+            node5.Next = node2;
+
+            var check = list.DetectCycle();
+            if (check)
+            {
+                Console.WriteLine("List has no cycle");
+            }
+            else
+            {
+                Console.WriteLine("List has cycles");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module4PooCursul1-Homework6/LinkedListDetectCycle/LinkedListDetectCycle/SingleLinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDetectCycle
+{
+    class SingleLinkedList
+    {
+        private Node First;
+
+        public Node AddFirst(int value)
+        {
+            Node node = new Node();
+
+            node.Value = value;
+            node.Next = null;
+
+            First = node;
+            return node;
+        }
+
+        public Node AddAfter(Node node, int value)
+        {
+            Node newNode = new Node();
+            newNode.Value = value;
+            node.Next = newNode;
+
+            return newNode;
+
+        }
+
+        public void PrintList()
+        {
+            Node currentNode = First;
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Value + " ");
+
+                currentNode = currentNode.Next;
+            }
+        }
+        public bool DetectCycle()
+        {
+            Node currentNode = First;
+
+            var listOfNodes = new List<Node>();
+            var item = 0;
+            while (currentNode != null)
+            {
+                item = listOfNodes.Count(elem => elem == currentNode.Next);
+
+                if (item > 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    listOfNodes.Add(currentNode.Next);
+                }
+
+                currentNode = currentNode.Next;
+            }
+            return true;
+        }
+    }
+}
