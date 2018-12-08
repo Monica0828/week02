33936a7f690fa80ddf1dd806e731192f8f5d6592
Monica0828 <danca.monica95@gmail.com>
2018-12-08 23:28:15 +0200

    Added ex 4

---
+++ Module4PooCursul1-Homework6/Ex4-Book/Ex4-Book/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_Book
+{
+    class Book
+    {
+        public Book (string title, string author, double price)
+        {
+            Author = author;
+            Title = title;
+            Price = price;
+        }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+
+        public string Print()
+        {
+            return $"Author of the book is {Author}, the title is {Title} and the price is {Price}";
+        }
+    }
+}
---
+++ Module4PooCursul1-Homework6/Ex4-Book/Ex4-Book/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfBooks = new List<Book>();
+            listOfBooks.Add(new Book("The cacher in the rye", "J.D. Salinger", 20.5));
+            listOfBooks.Add(new Book("Invitatie la dans", "Mihail Drumes",15.4));
+            listOfBooks.Add(new Book("The Alchemist", "Paulo Choelo", 50.5));
+            listOfBooks.Add(new Book("The Martian", "Andi Weir", 70.5));
+            listOfBooks.Add(new Book("The Ocean at the End of the Lane", "Neil Gaiman", 46.5));
+            int count = 0;
+            foreach(var elem in listOfBooks)
+            {
+                if(count==1 || count==3)
+                {
+                    elem.Price = elem.Price * 1.5;
+                    elem.Title = elem.Title.ToUpper();
+                    Console.WriteLine(elem.Print());
+                }
+                else
+                {
+                    Console.WriteLine(elem.Print());
+
+                }
+                count++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
